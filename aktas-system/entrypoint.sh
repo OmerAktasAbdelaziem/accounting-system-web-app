a7d49da2d2780ac24e305bf59113a680d4d8fbe9
OmerAktas <contact.us.omer@gmail.com>
2026-05-01 20:33:47 +0300

    Run migrations with --seed in entrypoint and Render preDeployCommand

--- aktas-system/entrypoint.sh
+++ aktas-system/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 # Retry migrations a few times to allow DB to become ready
 RETRIES=6
 COUNT=0
-until /usr/local/bin/php artisan migrate --force
+until /usr/local/bin/php artisan migrate --force --seed
 do
   COUNT=$((COUNT+1))
   if [ $COUNT -ge $RETRIES ]; then
