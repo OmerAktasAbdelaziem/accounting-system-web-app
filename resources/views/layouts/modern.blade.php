576bc14337ada927c9d8fc1a78be408eb8892597
OmerAktas <contact.us.omer@gmail.com>
2026-05-25 14:58:09 +0300

    a

--- resources/views/layouts/modern.blade.php
+++ resources/views/layouts/modern.blade.php
@@ -619,6 +619,8 @@
 
     <!-- Scripts -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- html2pdf (client-side PDF export) -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js"></script>
     <script>
         $.ajaxSetup({
             headers: {
